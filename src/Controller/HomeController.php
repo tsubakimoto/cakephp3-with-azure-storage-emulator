4458cc3325006f777e4721868a2ad872e21fa065
y-matsumura <y-matsumura@alterbooth.com>
2018-01-07 23:43:11 +0900

    [add] HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Home Controller
+ *
+ */
+class HomeController extends AppController
+{
+    public function index()
+    {
+    }
+}
